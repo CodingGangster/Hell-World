3443397065196a7b96b33badfe175df5a71f359f
yjt00 <yjt00@192.168.25.26>
2018-05-26 21:11:08 +0900

    Init Noob Project 20180526

---
+++ src/javaProject/Date.java
@@ -0,0 +1,12 @@
+package javaProject;
+
+public class Date {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello, world!");
+		
+		
+	}
+
+}
